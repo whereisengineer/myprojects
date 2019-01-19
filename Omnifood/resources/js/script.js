a2e245618f9d1f95731efa53454df55f3c9ed803
gigs <gaurav.kalra@cerner.com>
2019-01-19 23:35:41 +0530

    myfirstgitcommit
    Fiirst Commit Omnifood

---
+++ Omnifood/resources/js/script.js
@@ -0,0 +1,112 @@
+$(document).ready(function(){
+    
+    /*For the sticky navigation*/
+    
+    $('.js--section-features').waypoint(function(direction){
+        if(direction == "down")
+            {
+                $('nav').addClass('sticky');
+            }
+        else
+            {
+                $('nav').removeClass('sticky');
+            }
+        
+    },{
+        offset:'60px;'
+    });
+    
+    
+    /*Scroll on buttons */
+    $('.js--scroll-to-plans').click(function(){
+        
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000)
+        
+    })
+    
+    
+    
+     $('.js--scroll-to-start').click(function(){
+        
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
+        
+    })
+    
+    
+    
+    /*Navigation*/
+    $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+    
+    
+    
+    /*Animation on scroll*/
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    },{
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    });
+    
+    /*Mobile nav*/
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon ion-icon');
+        nav.slideToggle(200); 
+        if(icon.hasClass('mobile'))
+            {
+                icon.addClass('mobile2');
+               $("ion-icon").attr('name', "close-circle");
+                 $("ion-icon").removeAttr('name','menu');
+                icon.removeClass('mobile');
+            }
+        else
+            {
+                icon.addClass('mobile');
+                $("ion-icon").attr('name', "menu");
+                $("ion-icon").removeAttr('name', "close-circle");
+                icon.removeClass('mobile2');
+            }
+        
+    });
+    
+    
+    
+    
+});
